2ee1d2a13883cc7bf8ba288c60cdcea37640d8bf
Ilya Safronov <IlyaSafronov18@gmail.com>
2024-01-31 01:40:40 +0300

    styles step two plus toggle js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 'use strict';
+const dropdown_btns = document.querySelectorAll('.dropdown-btn');
 const burger = document.querySelector('.burger');
 burger.addEventListener('click', () => {
   const navbar_content = document.querySelector('.navbar-content');
   burger.classList.toggle('active');
 
   navbar_content.classList.toggle('show');
-})
+});
+
+dropdown_btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const dropdown = e.currentTarget.nextElementSibling;
+    dropdown.classList.toggle('show');
+    console.log(dropdown);
+  });
+});
